fcadb270bfb4f16494bb25bf84be4c68d42e37a5
samara04 <samara04@yandex.ru>
2022-02-26 16:06:17 +0400

    code revision Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,49 @@
 fun main() {
-    println("Введите имя")
-    val user = readLine()?.let { User(it) }
-    println("Количество секунд с момента выхода из сети?")
-    if (user != null) {
-        user.secAgo = readLine()?.toLong()!!
-        user.printText()
+
+    print("Введите имя\n")
+    val userName: String = readLine() ?: return
+
+    print("Количество секунд с момента выхода из сети?\n")
+    if (userName.isEmpty()) {
+        print("Пустая строка")
+    } else {
+        val secAgo = (readLine()?.toLong() ?: return)
+        printText(userName, secAgo.toInt())
+    }
+}
+
+
+fun printText(userName: String, secAgo: Int) {
+    println(message = "$userName был(а) в сети ${agoToText(secAgo)}")
+}
+
+private fun agoToText(secAgo: Int): String {
+
+    val secInMinutes = 60
+    val secInHours = 3_600
+    val secInDays = 86_400
+
+    return when (secAgo) {
+        in 0..secInMinutes -> "только что"
+        in secInMinutes + 1..secInHours ->
+            "${secAgo / secInMinutes} ${numDeclension((secAgo / secInMinutes),"минуту", "минут", "минуты")} назад"
+        in secInHours + 1..secInDays  ->
+            "${secAgo / secInHours} ${numDeclension((secAgo / secInMinutes),"час", "часов", "часа")} назад"
+        in secInDays  + 1..2 * secInDays  -> "сегодня"
+        in 2 * secInDays  + 1..3 * secInDays  -> "вчера"
+        else -> "давно"
+    }
+}
+
+private fun numDeclension(
+    num: Int,
+    one: String,
+    few: String,
+    many: String
+): String {
+    return when {
+        (num.mod(10) == 1) && (num.mod(100) != 11) -> one
+        (2 <= num.mod(10)) && (num.mod(10) <= 4) && ((num.mod(100) < 10) || (num.mod(100) >= 20)) -> many
+        else -> few
     }
 }
