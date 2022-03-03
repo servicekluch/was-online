a3571beb939644c0e2d644c31e5d297aad13e8f3
samara04 <samara04@yandex.ru>
2022-03-03 20:03:47 +0400

    code optimization Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,7 @@
+const val secondsInMinute = 60
+const val secondsInHour = 3_600
+const val secondsInDay = 86_400
+
 fun main() {
 
     print("Введите имя: ")
@@ -5,7 +9,7 @@ fun main() {
 
     if (!userName.isBlank()) {
         print("Количество секунд с момента выхода из сети? ")
-        printText(userName, (readLine()?.toLong() ?: return).toInt())
+        printText(userName, readLine()?.toInt() ?: return)
     }
 }
 
@@ -13,27 +17,15 @@ fun printText(userName: String, secAgo: Int) {
     println(message = "$userName был(а) в сети ${agoToText(secAgo)}")
 }
 
+
 private fun agoToText(secAgo: Int): String {
-
-    var secondsInMinute = 60
-    var secondsInHour = 3_600
-    var secondsInDay = 86_400
-    var minutes = secAgo / secondsInMinute
-    var hours = secAgo / secondsInHour
-
-    var minute_One = "минуту"
-    var minute_Few = "минуты"
-    var minute_Many = "минут"
-    var hour_One = "час"
-    var hour_Few = "часа"
-    var hour_Many = "часов"
 
     return when (secAgo) {
         in 0..secondsInMinute -> "только что"
         in secondsInMinute + 1..secondsInHour ->
-            "$minutes ${numDeclension(minutes, minute_One, minute_Few, minute_Many)} назад"
+            "${secAgo / secondsInMinute} ${numDeclension(secAgo / secondsInMinute, TimeType.Minutes)} назад"
         in secondsInHour + 1..secondsInDay  ->
-            "$hours ${numDeclension(hours, hour_One, hour_Few, hour_Many)} назад"
+            "${secAgo / secondsInHour}  ${numDeclension(secAgo / secondsInHour, TimeType.Hours)} назад"
         in secondsInDay + 1..2 * secondsInDay  -> "сегодня"
         in 2 * secondsInDay  + 1..3 * secondsInDay  -> "вчера"
         else -> "давно"
@@ -42,15 +34,29 @@ private fun agoToText(secAgo: Int): String {
 
 private fun numDeclension(
     num: Int,
-    one: String,
-    few: String,
-    many: String
+    timeType: TimeType
 ): String {
-    var divisionBy10 = num.mod(10)
-    var divisionBy100 = num.mod(100)
+    val divisionBy10 = num.mod(10)
+    val divisionBy100 = num.mod(100)
     return when {
-        (divisionBy10 == 1) && (divisionBy100 != 11) -> one
-        (2 <= divisionBy10) && (divisionBy10 <= 4) && ((divisionBy100 < 10) || (divisionBy100 >= 20)) -> few
-        else -> many
+        (divisionBy10 == 1) && (divisionBy100 != 11) ->
+            when (timeType) {
+                TimeType.Minutes -> "минуту"
+                TimeType.Hours -> "час"
+            }
+        (2 <= divisionBy10) && (divisionBy10 <= 4) && ((divisionBy100 < 10) || (divisionBy100 >= 20)) ->
+            when (timeType) {
+                TimeType.Minutes -> "минуты"
+                TimeType.Hours -> "часа"
+            }
+        else ->
+            when (timeType) {
+                TimeType.Minutes -> "минут"
+                TimeType.Hours -> "часов"
+            }
     }
+}
+
+private enum class TimeType {
+    Minutes, Hours
 }
