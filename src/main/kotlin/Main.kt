acd89ecf97813ab52ce5a4de7a9c11ebedbec5ed
samara04 <samara04@yandex.ru>
2022-03-10 20:22:33 +0400

    minor fixes after teacher's tips Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
-const val SECONDS_IN_MINUTE = 60
-const val SECONDS_IN_HOUR = 3_600
-const val SECONDS_IN_DAY = 86_400
+const val SECONDS_IN_MINUTE = 60u
+const val SECONDS_IN_HOUR = 3_600u
+const val SECONDS_IN_DAY = 86_400u
 
 fun main() {
 
@@ -14,49 +14,37 @@ fun main() {
 }
 
 fun printText(userName: String, secAgo: UInt) {
-    println(message = "$userName был(а) в сети ${agoToText(secAgo.toInt())}")
+    println(message = "$userName был(а) в сети ${agoToText(secAgo)}")
 }
 
 
-private fun agoToText(secAgo: Int): String {
+private fun agoToText(secAgo: UInt): String {
 
     return when (secAgo) {
-        in 0..SECONDS_IN_MINUTE -> "только что"
-        in SECONDS_IN_MINUTE + 1..SECONDS_IN_HOUR ->
-            "${secAgo / SECONDS_IN_MINUTE} ${numDeclension(secAgo / SECONDS_IN_MINUTE, TimeType.Minutes)} назад"
-        in SECONDS_IN_HOUR + 1..SECONDS_IN_DAY  ->
-            "${secAgo / SECONDS_IN_HOUR}  ${numDeclension(secAgo / SECONDS_IN_HOUR, TimeType.Hours)} назад"
-        in SECONDS_IN_DAY + 1..2 * SECONDS_IN_DAY  -> "сегодня"
-        in 2 * SECONDS_IN_DAY  + 1..3 * SECONDS_IN_DAY  -> "вчера"
+        in 0u..SECONDS_IN_MINUTE -> "только что"
+        in SECONDS_IN_MINUTE + 1u..SECONDS_IN_HOUR -> {
+            val minutes: UInt = secAgo / SECONDS_IN_MINUTE
+            "${minutes} ${numDeclension(minutes, "минуту", "минуты", "минут")} назад" }
+        in SECONDS_IN_HOUR + 1u..SECONDS_IN_DAY  -> {
+            val hours: UInt = secAgo / SECONDS_IN_HOUR
+            "${hours}  ${numDeclension(hours, "час", "часа", "часов")} назад" }
+        in SECONDS_IN_DAY + 1u..2u * SECONDS_IN_DAY  -> "сегодня"
+        in 2u * SECONDS_IN_DAY  + 1u..3u * SECONDS_IN_DAY  -> "вчера"
         else -> "давно"
     }
 }
 
 private fun numDeclension(
-    num: Int,
-    timeType: TimeType
+    num: UInt,
+    one: String,
+    few: String,
+    many: String
 ): String {
-    val divisionBy10 = num.mod(10)
-    val divisionBy100 = num.mod(100)
+    val divisionBy10: UInt = num.mod(10u)
+    val divisionBy100: UInt = num.mod(100u)
     return when {
-        (divisionBy10 == 1) && (divisionBy100 != 11) ->
-            when (timeType) {
-                TimeType.Minutes -> "минуту"
-                TimeType.Hours -> "час"
-            }
-        (2 <= divisionBy10) && (divisionBy10 <= 4) && ((divisionBy100 < 10) || (divisionBy100 >= 20)) ->
-            when (timeType) {
-                TimeType.Minutes -> "минуты"
-                TimeType.Hours -> "часа"
-            }
-        else ->
-            when (timeType) {
-                TimeType.Minutes -> "минут"
-                TimeType.Hours -> "часов"
-            }
+        (divisionBy10 == 1u) && (divisionBy100 != 11u) -> one
+        (2u <= divisionBy10) && (divisionBy10 <= 4u) && ((divisionBy100 < 10u) || (divisionBy100 >= 20u)) -> few
+        else -> many
     }
-}
-
-private enum class TimeType {
-    Minutes, Hours
 }
